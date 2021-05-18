5e6430cbb4912ac78c260b7cf1e78ad5b17e6030
Akshar Kommajosyula <aksharkomm@gmail.com>
2021-05-18 17:05:28 +0530

    added login/signup features

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
-var prodSchema = require('./models/product');
+const bcrypt = require('bcryptjs');
+var prodSchema = require('./models/Product');
+const User = require('./models/User');
+const passport = require('passport');
+const { ensureAuthenticated } = require('./config/auth');
 
 module.exports = function(app){
     app.get('/products', async (req,res) => {
@@ -10,6 +14,99 @@ module.exports = function(app){
     app.get('/', (req,res) => {
         res.render('index');
     });
+
+
+    app.post('/register', (req,res) => {
+        //console.log(req.body);
+        const { firstname, lastname, email, contact, password, password2 } = req.body;
+        let errors = [];
+        // console.log({ firstname, lastname, email, contact, password, password2 });
+
+        //Check if any fields are empty
+        if(!firstname || !lastname || !email || !contact || !password || !password2){
+            errors.push({ msg: 'Please fill in all fields.' });
+        }
+
+        //Check if passwords match
+        if(password !== password2){
+            errors.push({ msg: 'Passwords do no match.' })
+        }
+
+        //Check password length
+        if(password.length < 6){
+            errors.push({ msg: 'Password should be atleast 6 characters.'})
+        }
+
+        if(errors.length > 0){
+            // console.log(errors);
+            res.render('signup', {
+                errors,
+                firstname,
+                lastname,
+                email,
+                contact,
+                password,
+                password2,
+            });
+        }
+        else{
+            User.findOne({email: email}).then(user => {
+                if(user){
+                    errors.push({msg: 'E-mail already exists.'})
+                    res.render('signup', {
+                        errors,
+                        firstname,
+                        lastname,
+                        email,
+                        contact,
+                        password,
+                        password2,
+                    });
+                }
+                else{
+                    const newUser = new User({
+                        firstName: firstname,
+                        lastName: lastname,
+                        email,
+                        password,
+                        contact,
+                    });
+
+                    bcrypt.genSalt(10, (err,salt) => 
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err) throw err;
+
+                            newUser.password = hash;
+                            newUser.save()
+                            .then((user) => {
+                                req.flash('success_msg', 'Sign up successful! You can now login.')
+                                res.redirect('/login');
+                            })
+                            .catch(err => console.log(err));
+                    }))
+                }
+            })
+        }
+        
+    });
+    app.post('/auth', (req,res,next) => {
+        passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true,
+        })(req,res,next);
+    });
+    app.get('/logout', (req,res) => {
+        req.logout();
+        req.flash('success_msg', 'You have logged out.');
+        re.redirect('login');
+    })
+
+
+
+    app.get('/logout', (req,res) => {
+        res.render('logout');
+    });
     app.get('/contactus', (req,res) => {
         res.render('contactus');
     });
@@ -19,55 +116,47 @@ module.exports = function(app){
     app.get('/signup', (req,res) => {
         res.render('signup');
     });
-    app.get('/cart', (req,res) => {
+    app.get('/cart', ensureAuthenticated, (req,res) => {
         res.render('cart');
     });
-    app.get('/wishlist', (req,res) => {
+    app.get('/wishlist', ensureAuthenticated, (req,res) => {
         res.render('wishlist');
     });
     app.get('/beaded', async (req,res) => {
         var prods = await prodSchema.find({category: 'Beaded'});
-        //console.log(prods);
         res.render('categories/beaded', {'prods': prods});
     });
     app.get('/bracelets', async (req,res) => {
         var prods = await prodSchema.find({category: 'Bracelets'});
-        //console.log(prods);
         res.render('categories/bracelets', {'prods': prods});
     });
     app.get('/earcuffs', async (req,res) => {
         var prods = await prodSchema.find({category: 'Earcuffs'});
-        //console.log(prods);
         res.render('categories/ear_cuffs', {'prods': prods});
     });
     app.get('/necklaces', async (req,res) => {
         var prods = await prodSchema.find({category: 'Necklaces'});
-        //console.log(prods);
         res.render('categories/necklaces', {'prods': prods});
     });
     app.get('/pearls', async (req,res) => {
         var prods = await prodSchema.find({category: 'Pearls'});
-        //console.log(prods);
         res.render('categories/pearls', {'prods': prods});
     });
     app.get('/polymerclay', async (req,res) => {
         var prods = await prodSchema.find({category: 'Polymer Clay'});
-        //console.log(prods);
         res.render('categories/polymer_clay', {'prods': prods});
     });
     app.get('/tassels', async (req,res) => {
         var prods = await prodSchema.find({category: 'Tassels'});
-        //console.log(prods);
         res.render('categories/tassels', {'prods': prods});
     });
     app.get('/threads', async (req,res) => {
         var prods = await prodSchema.find({category: 'Threads'});
-        //console.log(prods);
         res.render('categories/threads', {'prods': prods});
     });
     app.get('/wiredearrings', async (req,res) => {
         var prods = await prodSchema.find({category: 'Wired Earrings'});
-        //console.log(prods);
         res.render('categories/wired_earrings', {'prods': prods});
     });
+
 };
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,42 @@
 const express = require('express')
 const connectDB = require('./config/db')
-const cors = require('cors')
-const morgan = require('morgan')
+const passport = require('passport')
+const flash = require('connect-flash')
+const session = require('express-session')
 require('dotenv').config()
-var productsController = require('./app')
+
 const app = express()
+
+require('./config/passport')(passport)
+
+var controller = require('./app')
 
 connectDB()
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
-}
-
-app.use(cors())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-productsController(app)
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  }))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    res.locals.error = req.flash('error')
+    next()
+});
+
+controller(app)
 
 const PORT = process.env.PORT || 3000
 
