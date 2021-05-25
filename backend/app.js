014948a054d2880f0c6216fc2329bd35d9439962
Akshar Kommajosyula <aksharkomm@gmail.com>
2021-05-26 00:30:39 +0530

    added more wishlist and cart related routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const User = require('./models/User');
 const passport = require('passport');
+var mongoose = require('mongoose');
 const { ensureAuthenticated } = require('./config/auth');
 const Product = require('./models/Product');
 var Cart = require('./models/carts')
@@ -24,8 +25,15 @@ module.exports = function(app){
         req.flash('success_msg', 'Logout successful!');
         res.redirect('login');
     });
-    app.get('/wishlist', ensureAuthenticated, (req,res) => {
-        res.render('wishlist');
+    app.get('/wishlist', ensureAuthenticated, async (req,res) => {
+        wishlist = req.user.wishlist;
+        var prods = [];
+        for(let name of wishlist){
+            var prod = await Product.findOne({name: name});
+            prods.push(prod);
+        }
+        // console.log(prods);
+        res.render('wishlist', {'prods': prods});
     });
     app.post('/auth', (req,res,next) => {
         passport.authenticate('local', {
@@ -40,33 +48,86 @@ module.exports = function(app){
             if(err){
                 return res.redirect('back');
             }
-            res.render('product_details', {product:product});
-        })
+            try{
+                res.render('product_details', {'product':product, 'wishlist': req.user.wishlist});
+            }
+            catch(err){
+                res.render('product_details', {'product':product});
+            }
+        });
     })
     app.get('/add-to-cart/:id', (req,res,next) => {
         var productID = req.params.id;
         var cart = new Cart(req.session.cart ? req.session.cart : {});
 
+        Product.updateOne({_id: productID}, {$inc: {stock: -1}}, (err,product) => {
+            if(err){
+                req.flash('error_msg','Error. Please try again.')
+                return res.redirect('back');
+            }
+        });
         Product.findById(productID, (err,product) => {
             if(err){
-                return res.redirect('/');
+                req.flash('error_msg','Error. Please try again.')
+                return res.redirect('back');
             }
             cart.add(product, product._id);
             req.session.cart = cart;
-            // console.log(req.session.cart);
-            req.flash('success_msg','Item added to cart successfully!')
+            if(req.user.wishlist.includes(product.name)){
+                User.updateOne({email: req.user.email}, {$pullAll: {wishlist: [product.name]}}, (err,product) => {
+                    if(err){
+                        req.flash('error_msg','Error. Please try again.')
+                        return res.redirect('back');
+                    }
+                });
+            }
+            req.flash('success_msg','Item added to cart!')
             res.redirect('back');
         });
-    });
+    })
+    app.get('/add-to-wishlist/:name', ensureAuthenticated, (req,res,next) => {
+        User.updateOne({email: req.user.email}, {$push: {wishlist: [req.params.name]}}, (err,product) => {
+            if(err){
+                req.flash('error_msg','Error. Please try again.')
+                return res.redirect('/');
+            }
+            req.flash('success_msg','Item removed from wishlist!')
+            res.redirect('back');
+        });
+    })
+    app.get('/remove-from-wishlist/:name', ensureAuthenticated, (req,res,next) => {
+        User.updateOne({email: req.user.email}, {$pullAll: {wishlist: [req.params.name]}}, (err,product) => {
+            if(err){
+                req.flash('error_msg','Error. Please try again.')
+                return res.redirect('/');
+            }
+            req.flash('success_msg','Item removed from wishlist!')
+            res.redirect('back');
+        });
+    })
     app.get('/reduce/:id', (req,res,next) => {
         var productID = req.params.id;
+        Product.updateOne({_id: productID}, {$inc: {stock: 1}}, (err,product) => {
+            if(err){
+                req.flash('error_msg','Error. Please try again.')
+                return res.redirect('back');
+            }
+        });
         var cart = new Cart(req.session.cart ? req.session.cart : {});
         cart.reduceByOne(productID);
         req.session.cart = cart;
         res.redirect('/cart');
     })
     app.get('/remove-from-cart/:id', (req,res,next) => {
+        var cart = new Cart(req.session.cart);
         var productID = req.params.id;
+        var incQty = cart.items[productID].qty;
+        Product.updateOne({_id: productID}, {$inc: {stock: incQty}}, (err,product) => {
+            if(err){
+                req.flash('error_msg','Error. Please try again.')
+                return res.redirect('back');
+            }
+        });
         var cart = new Cart(req.session.cart);
         cart.removeItem(productID);
         req.session.cart = cart;
@@ -75,33 +136,45 @@ module.exports = function(app){
     });
     app.get('/clear-cart', (req,res,next) => {
         var cart = new Cart(req.session.cart);
+        for(const property in cart.items){
+            var productID = cart.items[property].item._id
+            var incQty = cart.items[property].qty
+            Product.updateOne({_id: productID}, {$inc: {stock: incQty}}, (err,product) => {
+                if(err){
+                    req.flash('error_msg','Error. Please try again.')
+                    return res.redirect('back');
+                }
+            });
+        }
         req.session.cart = null;
         res.redirect('/cart');
         
     });
-    app.get('/cart', (req,res) => {
+    app.get('/cart', async (req,res) => {
         if(!req.session.cart){
             return res.render('cart', {products: null});
         }
         var cart = new Cart(req.session.cart);
         var prods = [];
+        var prod = {};
         for(const property in cart.items){
             var prod = {};
+            var product = await Product.findById(property);
             prod = {
                 '_id': cart.items[property].item._id,
                 'name': cart.items[property].item.name,
                 'price': cart.items[property].item.price,
                 'qty': cart.items[property].qty,
                 'total': cart.items[property].price,
-                'path': cart.items[property].item.imgPath
+                'path': cart.items[property].item.imgPath,
+                'stock': product.stock
             }
             prods.push(prod);
         }
 
         res.render('cart', {products: prods, totalPrice: cart.totalPrice});
     });
-    // app.post('/checkout', req)
-    app.all('/checkout', ensureAuthenticated, (req,res,next) => {
+    app.get('/checkout', ensureAuthenticated, (req,res,next) => {
         if(!req.session.cart){
             return res.redirect('/cart');
         }
@@ -120,8 +193,9 @@ module.exports = function(app){
             prods.push(prod);
         }
         var order = new Order({
+            email: req.user.email,
             cart: prods,
-            note: note,
+            // note: note,
             totalAmount: cart.totalPrice,
             totalQty: cart.totalQty,
         });
